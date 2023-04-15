1c7c60c63d7e0a6c23ca2b230aa06e832210c4b3
Sebastian <69520725+tillseb@users.noreply.github.com>
2023-04-15 18:05:49 +0200

    Tinkering with time.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
+import * as React from 'react';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ data }: { data: { time: string } }) {
+
+  const [time, setTime] = React.useState<Date | null>(null);
+  React.useEffect(() => {
+      fetch('/api/time')
+      .then(res => res.json())
+      .then(json => setTime(new Date(json.time)));
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -11,6 +20,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.tsx</code>
         </p>
+        {time &&
+                    `The time is ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`}
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
